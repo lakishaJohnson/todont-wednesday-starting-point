b6bd8f739ef34f1934f3cd61b4efdcb58d47e88d
Lakisha J <lakishajohnson@pursuit.org>
2021-10-27 11:32:10 -0400

    Final update

--- todont.js
+++ todont.js
@@ -1,29 +1,29 @@
 const readlineSync = require("readline-sync");
 const chalk = require("chalk");
 const getListsMenu = require("./getListsMenu");
-const lists = require("./lists");
 const viewList = require("./viewList");
+const app = require("./app");
 
 console.clear();
+//ASK USER WHAT LIST THEY WANT TO VIEW OR CREATE
 let response = readlineSync.question(getListsMenu()).toLowerCase();
-
+//LOOP THRU LIST UNTIL "q" IS SELECTED
 while (response !== "q") {
   if (isNaN(response) === false) {
-    const list = lists[response - 1];
+    //IF USER RESPONSE IS A NUMBER, THAT LIST IS VIEWED
+    const list = app.lists[response - 1];
     viewList(list);
   } else if (response === "n") {
     console.clear();
-    // get the text from the user
-    const text = readlineSync.question("What list do you want to add?\n\n");
-    // make a new todo out of it
-    const todo = makeTodo(text);
-    // put it in our todo list
-    todos.unshift(todo);
+    // IF RESPONSE IS "n", ASK USER ANOTHER QUESTION
+    const title = readlineSync.question("What list do you want to add?\n\n");
+    // ADD NEW TODO TO LIST
+    app.addList(title)
     console.clear();
   } else {
+    console.clear()
     console.log(chalk.red.bold(`${response} is not a valid input.`));
   }
-
-  console.clear();
+//ASK USER ANOTHER QUESTION
   response = readlineSync.question(getListsMenu()).toLowerCase();
 }
